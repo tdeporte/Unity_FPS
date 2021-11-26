23f3ab5c5d0777a9c31c4b8c46ee827a7e500db7
tdeporte <tom.deporte@etu.u-bordeaux.fr>
2021-11-26 20:27:55 +0100

    Pathfinding du pauvre

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,8 +7,8 @@ public class Player : MonoBehaviour
     //public float gravity = 5;
     public float translation;
     public float rotation;
-    public float speed = 1.0f;
-    public float rotationSpeed = 0.001f;
+    public float c_speed = 0.1f;
+    public float rotationSpeed = 100f;
     private Rigidbody  rigibBodyComponent;
 
     //SAUT
@@ -17,37 +17,59 @@ public class Player : MonoBehaviour
 
     public Vector3 destination;
 
+    private float distance = 2f;
+
+    private int repel_force = 20;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rigibBodyComponent = gameObject.GetComponent<Rigidbody>() ;
-        destination = new Vector3(1f,0.5f,-15f); 
+        destination = new Vector3(1f,1.5f,-15f); 
     }
 
     // Update is called once per frame
     
+
     void Update()
     {
+        Vector3 position = rigibBodyComponent.position;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * distance;
+        Vector3 left = transform.TransformDirection(Vector3.left) * distance;
+        Vector3 right = transform.TransformDirection(Vector3.right) * distance;
+
+        Vector3 dir = (destination - position).normalized;
+
+        Vector3[] rays = {forward,left,right};
         
+        for(int i =0;i<rays.Length;i++){
+            Debug.DrawRay(transform.position, rays[i], Color.red);
+        }
         
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, forward, out hit,distance)){
+            dir += hit.normal * repel_force; // repel_force is force to repel by
+        }
+
+        if (Physics.Raycast(left, forward, out hit,distance)){
+            dir += hit.normal * repel_force; // repel_force is force to repel by
+        }
+
+        if (Physics.Raycast(right, forward, out hit,distance)){
+            dir += hit.normal * repel_force; // repel_force is force to repel by
+        }
+
+        // rotation
+        Quaternion  rot = Quaternion.LookRotation (dir);
+        //print ("rot : " + rot);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime);
+        //position
+        transform.position += transform.forward * (2 * Time.deltaTime); // 20 is speed
     }
 
     void FixedUpdate () {
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        //rigibBodyComponent.velocity = new Vector3 (horizontal * speed, rigibBodyComponent.velocity.y, vertical * speed);
-        
-        Vector3 position = rigibBodyComponent.position;
-
-        var fwd = transform.TransformDirection (Vector3.forward);
-        RaycastHit hit;
-
-        if (!Physics.Raycast(transform.position, fwd, out hit,2)){
-            Debug.Log("Found an object - distance: " + hit.distance);
-            float step =  speed * Time.deltaTime; // calculate distance to move
-            rigibBodyComponent.position = Vector3.MoveTowards(position, destination, step);
-        }
         
     }
 
